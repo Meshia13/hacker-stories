bab6d4c9cd7c8afbbe892a8b83006abebe6df927
Carmeshia Lazzana <65563803+Meshia13@users.noreply.github.com>
2024-10-31 20:23:32 -0400

    Embedded JS in JSX, cleared CSS files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
 
+const title = "React";
+
 function App() {
 
   return (
     <div>
-      <h1>Hello React</h1>
+      <h1>Hello {title}</h1>
+      
+      <label htmlFor="search">Search: </label>
+      <input type="text" id="search" />
     </div>
   );
 
