86d1688c2758655d051919e5547b9d561451281c
Carmeshia Lazzana <65563803+Meshia13@users.noreply.github.com>
2024-11-22 13:48:37 -0500

    created event handlers and useState for app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,54 @@
 import * as React from 'react';
 
+// const list = [
+//   {
+//     title: "React",
+//     url: 'https://reactjs.org/',
+//     author: "Jordan Walke",
+//     num_comment: 3,
+//     points: 4,
+//     objectId: 0,
+//   },
+//   {
+//     title: "Chocolate",
+//     url: 'https://redux.js.org/',
+//     author: "Willy Wonka, Charlie Boy",
+//     num_comment: 2,
+//     points: 5,
+//     objectId: 1,
+//   }
+// ]
 
-
-const list = [
-  {
-    title: "React",
-    url: 'https://reactjs.org/',
-    author: "Jordan Walke",
-    num_comment: 3,
-    points: 4,
-    objectId: 0,
-  },
-  {
-    title: "Chocolate",
-    url: 'https://redux.js.org/',
-    author: "Willy Wonka, Charlie Boy",
-    num_comment: 2,
-    points: 5,
-    objectId: 1,
-  }
-]
 
 {/* creating App component */}
 // arrow function expression
 const App = () => {
+
+  const stories = [
+    {
+      title: "React",
+      url: 'https://reactjs.org/',
+      author: "Jordan Walke",
+      num_comment: 3,
+      points: 4,
+      objectId: 0,
+    },
+    {
+      title: "Chocolate",
+      url: 'https://redux.js.org/',
+      author: "Willy Wonka, Charlie Boy",
+      num_comment: 2,
+      points: 5,
+      objectId: 1,
+    }
+  ]
+
+  //  A callback handler gets  introduced as event handler (A)
+  const handleSearch =(event)=> {
+    // and calls back to the place it was introduced (D)
+    console.log(event.target.value)
+
+  }
 
   return (
     <div>
@@ -32,20 +57,23 @@ const App = () => {
         My Hacker Stories
       </h1>
 
-      {/* creating an instance of Search component */}
-      <Search />
+      {/* handler is passed as function in props to another component (B) */}
+      <Search onSearch={handleSearch} />
 
       <hr />
-      {/* creating an instance of List component */}
-      <List />
+      {/* creating an instance of List component 
+        passing stories in the List component */}
+      <List list ={stories}/>
+      
 
     </div>
   );
 
 }
 
-{/* creating List component */}
-const List = () => {
+{/* creating List component
+   retrieve the list from the List component’s function */}
+const List = (props) => {
 
   return (
 
@@ -53,20 +81,13 @@ const List = () => {
       
       <ul>
       
-      {/* arrow function expression refractoring would look like "list.map((item) {...})" */}
-        {list.map(function (item) {
-          return ( 
+      {/* arrow function expression refractoring would look like "list.map((item) {...})"
+       retrieve the list from the List component’s function */}
+        {props.list.map(function (item) {
 
-            <li key={item.objectId}>
+          return (
 
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comment}</span>
-              <span>{item.points}</span>
-              
-            </li>
+            <Item key={item.objectId} item={item} />
           )
         })}
       </ul>
@@ -76,15 +97,40 @@ const List = () => {
   )
 }
 
+// extract an Item component from the List component and pass each item in the map() method’s callback function to this new component. 
+const Item = (props) => {
+
+  return ( 
+
+    <li >
+
+      <span>
+        <a href={props.item.url}>{props.item.title}</a>
+      </span>
+      <span>{props.item.author}</span>
+      <span>{props.item.num_comment}</span>
+      <span>{props.item.points}</span>
+      
+    </li>
+  )
+
+}
+
 {/* creating Search component */}
-const Search = () => {
+const Search = (props) => {
+
+  const [searchTerm, setSearchTerm] = React.useState("");
 
   // Event Handler
   const handleChange = (event) => {
-    console.log(event);
+    // Re-renders the component as the state changes
+    setSearchTerm(event.target.value);
+
+     // is executed there as callback handler (C)
+    props.onSearch(event);
   }
   
-
+  
     return (
       <>
 
@@ -94,6 +140,10 @@ const Search = () => {
       {/* clears the input field */}
       <input type='reset'/>
     </form>
+
+    <p>
+      {searchTerm}
+    </p>
 
     </>
   )
