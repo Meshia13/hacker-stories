dbec3ab371ae4312a3182aca99edaf476ff1cf8e
Carmeshia Lazzana <65563803+Meshia13@users.noreply.github.com>
2024-11-21 19:13:33 -0500

    updating app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import * as React from 'react';
 
-const title = "React";
+function getTitle(title) {
+  return title;
+}
+
+const list = [
+  {
+    title: "React",
+    author: "Jordan Walke",
+    num_comment: 3,
+    points: 4,
+    objectId: 0,
+  },
+  {
+    title: "Chocolate",
+    author: "Willy Wonka, Charlie Boy",
+    num_comment: 2,
+    points: 5,
+    objectId: 1,
+  }
+]
 
 function App() {
 
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>
+        Hello {getTitle("React")}
+      </h1>
       
       <label htmlFor="search">Search: </label>
       <input type="text" id="search" />
